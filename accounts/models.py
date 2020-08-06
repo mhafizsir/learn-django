e8782b57c8ed41304d73418b551e973d8d5e04d3
mhafizsir <mhafizsir@gmail.com>
2020-08-06 21:22:42 +0700

    upload image done

--- accounts/models.py
+++ accounts/models.py
@@ -6,11 +6,11 @@ from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, null=True)
     phone = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
-    profile_pic = models.ImageField(null=True, blank=True)
+    profile_pic = models.ImageField(default="1200px-Circle-icons-profile.svg.png", null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
